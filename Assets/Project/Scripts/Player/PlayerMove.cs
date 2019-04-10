b460474328b05ae1404315393b73ebeec1c8b936
Xrocetoxtos <tommyvanson@gmail.com>
2019-04-10 23:56:26 +0200

    10-4-2019
    crouching

--- Assets/Project/Scripts/Player/PlayerMove.cs
+++ Assets/Project/Scripts/Player/PlayerMove.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private PlayerState playerState;
     public float moveSpeed = 6f;
     public float backBrake = 0.67f;
+    public float crouchBrake = .5f;
     public float sprintMultiplier = 2f;
-    private bool isRunning = false;
     private float actualMoveSpeed;
 
     private float moveTranslation;
     private float moveStraffe;
+
+    private void Awake()
+    {
+        playerState = GetComponent<PlayerState>();
+    }
 
     private void Update()
     {
@@ -27,21 +33,55 @@ public class PlayerMove : MonoBehaviour
     {
         moveTranslation = Input.GetAxis("Vertical");
         moveStraffe = Input.GetAxis("Horizontal");
+
+        PlayerRun();
+        PlayerCrouch();
+        PlayerJump();
+    }
+
+    private void PlayerRun()
+    {
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            isRunning = true;
+            playerState.isRunning = true;
         }
         else
         {
-            isRunning = false;
+            playerState.isRunning = false;
         }
+    }
+
+    private void PlayerCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Debug.Log(playerState.isCrouching);
+
+            if (!playerState.isCrouching)
+            {
+                playerState.PlayerCrouching();
+            }
+            else
+            {
+                playerState.PlayerStanding();
+            }
+            Debug.Log(playerState.isCrouching);
+
+
+
+        }
+    }
+
+    private void PlayerJump()
+    {
+
     }
 
     private float GetMoveSpeed()
     {
         float speed = moveSpeed;
 
-        if(isRunning)
+        if(playerState.isRunning)
         {
             speed *= sprintMultiplier;
         }
@@ -49,6 +89,10 @@ public class PlayerMove : MonoBehaviour
         if(moveTranslation<0f)
         {
             speed *= backBrake;
+        }
+        if(playerState.isCrouching)
+        {
+            speed *= crouchBrake;
         }
 
         return speed;
---
+++ Assets/Project/Scripts/Player/PlayerState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public bool isRunning = false;
+    public bool isCrouching = false;
+    public GameObject standingPlayer;
+    public GameObject crouchingPlayer;
+
+    private void Awake()
+    {
+
+        
+    }
+
+    public void PlayerStanding()
+    {
+        isCrouching = false;
+        SetActiveState();
+    }
+
+    public void PlayerCrouching()
+    {
+        isCrouching = true;
+        SetActiveState();
+    }
+
+    private void SetActiveState()
+    {
+        standingPlayer.SetActive(!isCrouching);
+        crouchingPlayer.SetActive(isCrouching);
+    }
+}
+
