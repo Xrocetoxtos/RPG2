b1e6bcc849d26926356808fe10b272803ec517aa
Xrocetoxtos <tommyvanson@gmail.com>
2019-04-21 00:37:52 +0200

    20-4
    Beginnetje aan de minimap. NESW moeten nog draaien

---
+++ Assets/Project/Scripts/GUI/WindDirections.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindDirections : MonoBehaviour
+{
+    private Quaternion rotation;
+
+    private void Awake()
+    {
+        rotation = transform.rotation;
+    }
+
+    void FixedUpdate()
+    {
+        transform.rotation = rotation;
+    }
+}
